db8739ef5a75ed95a6eb5dca785ec620da01e184
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2023-06-15 16:30:42 +0200

    Added search providers & <head>

--- islands/Search.tsx
+++ islands/Search.tsx
@@ -1,4 +1,4 @@
-import { useState } from "preact/hooks";
+import { useEffect, useState } from "preact/hooks";
 import Input from "../components/Input.tsx";
 
 type Suggestions = {
@@ -7,8 +7,8 @@ type Suggestions = {
   image?: string;
 }[];
 
-async function callAPI(query: string): Promise<Suggestions> {
-  const url = `https://searchsuggestions.netlify.app/google/en/${query}`;
+async function callAPI(query: string, provider: string): Promise<Suggestions> {
+  const url = `https://searchsuggestions.netlify.app/${provider}/en/${query}`;
   const resp = await fetch(url);
 
   if (resp.status === 404) {
@@ -22,25 +22,60 @@ async function callAPI(query: string): Promise<Suggestions> {
 export default function Search() {
   const [list, setList] = useState([] as Suggestions);
   const [query, setQuery] = useState("");
+  const [provider, setProvider] = useState("google");
+  const providers = [
+    "google",
+    "bing",
+    "yahoo",
+    "qwant",
+    "duckduckgo",
+    "startpage",
+  ];
 
-  async function handleInput(e: Event) {
+  function handleInput(e: Event) {
     const val = (e.target as HTMLInputElement).value;
     setQuery(val);
+  }
 
-    const json = await callAPI(val);
+  function handleProvider(e: Event) {
+    const val = (e.target as HTMLSelectElement).value;
+    setProvider(val);
+  }
+
+  async function handleList(q: string) {
+    const json = await callAPI(q, provider);
     setList(json);
   }
 
+  useEffect(() => {
+    handleList(query);
+  }, [query]);
+
   return (
     <>
-      <Input
-        type="text"
-        name="searchbar"
-        value={query}
-        onInput={handleInput}
-        placeholder="Search with Google"
-        class="w-full rounded-md focus:border-red-400 focus:outline-none"
-      />
+      <div class="flex flex-col-reverse gap-2 sm:flex-row">
+        <Input
+          type="text"
+          name="searchbar"
+          value={query}
+          onInput={handleInput}
+          placeholder="Search something "
+          class="w-full rounded-md focus:border-red-400 focus:outline-none"
+        />
+
+        <select
+          name="providers"
+          value={provider}
+          onChange={handleProvider}
+          class="bg-transparent border(gray-500 2) rounded-md px-3 py-2"
+        >
+          {providers.map((item) => (
+            <option value={item}>
+              {item}
+            </option>
+          ))}
+        </select>
+      </div>
 
       {list.length > 0 && (
         <ul class="my-4 w-full border-2 rounded-md">
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,17 +1,30 @@
+import { Head } from "$fresh/runtime.ts";
 import Search from "../islands/Search.tsx";
 
 export default function Home() {
   return (
-    <div class="container h-screen pb-16 flex flex-col">
-      <div class="mx-auto max-w-lg w-full px-5">
-        <header class="w-full my-16 text-left">
-          <h1 class="text-3xl font-black leading-7 text-red-400">
-            Wow, free suggestions from multiple providers ??
-          </h1>
-        </header>
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <title>Search Suggestions API | Example page</title>
+        <meta
+          name="description"
+          content="You guys get free suggestions from multiple search engine providers ?"
+        />
+      </Head>
 
-        <Search />
+      <div class="container columns-3 h-screen pb-16 flex">
+        <div class="mx-auto max-w-lg w-full px-5">
+          <header class="w-full my-8 sm:my-16 text-left bg-red-50 rounded">
+            <h1 class="text-2xl sm:text-3xl font-black leading-7 text-red-400">
+              Wow, free suggestions from multiple search engines !?
+            </h1>
+          </header>
+
+          <Search />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
