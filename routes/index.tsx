de14ea06bd7726a543a9e408a2aa1c7d180b996e
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2023-06-15 18:15:16 +0200

    Added links to relevent repos

--- routes/index.tsx
+++ routes/index.tsx
@@ -16,8 +16,26 @@ export default function Home() {
 
       <div class="container columns-3 h-screen pb-16 flex">
         <div class="mx-auto max-w-lg w-full px-5">
-          <header class="w-full my-8 sm:my-16 text-left bg-red-50 rounded">
-            <h1 class="text-2xl sm:text-3xl font-black leading-7 text-red-400">
+          <header class="w-full my-8 sm:my-16 text-left">
+            <div class="flex my-2">
+              <a
+                class="flex align-center p-2 gap-2 text-gray-600 text-sm hover:underline"
+                href="https://github.com/victrme/search-suggestions"
+              >
+                <img src="/github.svg" width="16" height="16" alt="" />
+                api used
+              </a>
+
+              <a
+                class="flex align-center p-2 gap-2 text-gray-600 text-sm hover:underline"
+                href="https://github.com/victrme/search-suggest-example"
+              >
+                <img src="/github.svg" width="16" height="16" alt="" />
+                this page
+              </a>
+            </div>
+
+            <h1 class="text-2xl sm:text-3xl font-black leading-7 text-red-400 bg-red-50 rounded">
               Wow, free suggestions from multiple search engines !?
             </h1>
           </header>
